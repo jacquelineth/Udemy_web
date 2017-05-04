24c93c388a38c9b09bfd3fd000a2d8dc667ae460
Thierry JACQUELINE <jacqueline@gmail.com>
2017-05-04 02:27:56 +0200

    JS String 101 Section 3, Lecture 43

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,8 @@
-console.log("hello John");
+var myString = "Hello John";
+/** index of position */
+var position = myString.indexOf("John");
+console.log(position); 
+
+var myNewString=  myString.replace("John", "Tom");
+
+console.log(myNewString); 
